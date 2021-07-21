13a0a9a58675e374116d44cea9cd0c8737276eb2
dieploc <loc904620@gmail.com>
2021-07-21 16:43:53 +0700

    init project v3

---
+++ dotnet_5_server/dotnet_5_server/Controllers/ErrorController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace dotnet_5_server.Controllers
+{
+    [AllowAnonymous]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class ErrorController : ControllerBase
+    {
+        [Route("error")]
+        public async Task Error()
+        {
+            var exception = HttpContext.Features.Get<IExceptionHandlerPathFeature>().Error;
+            var code = 500;
+            var message = filterError(exception.InnerException?.Message ?? exception.Message);
+            var response = JsonSerializer.Serialize(
+                        new { message = message, error = exception.Message, statusCode = code });
+            HttpContext.Response.StatusCode = code;
+            HttpContext.Response.ContentType = "application/json";
+            await HttpContext.Response.WriteAsync(response);
+        }
+
+        private string filterError(string message)
+        {
+            if (message.Contains("UNIQUE constraint failed"))
+            {
+                string targetField;
+                string[] messageArray = message.Split("'");
+                if (messageArray.Length < 2) return message;
+
+                string content = messageArray[1];
+                if (content.Contains("."))
+                {
+                    var fieldArray = content.Split(".");
+                    targetField = fieldArray[fieldArray.Length - 1];
+                }
+                else
+                {
+                    var fieldArray = content.Split(":");
+                    targetField = fieldArray[fieldArray.Length - 1];
+                }
+                return targetField.ToUpper() + " already exists";
+            }
+            return message;
+        }
+    }
+}
---
+++ dotnet_5_server/dotnet_5_server/Startup.cs
@@ -0,0 +1,76 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_5_server
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("ClientPermission", policy =>
+                {
+                    policy.AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .WithOrigins("http://localhost:3000")
+                        .AllowCredentials();
+                });
+            });           
+
+            services.AddControllers()
+                .AddNewtonsoftJson(options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+            
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "dotnet_5_server", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                //app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "dotnet_5_server v1"));
+            }
+            app.UseExceptionHandler("/error");
+
+            app.UseHttpsRedirection();
+
+            app.UseCors("ClientPermission");
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
